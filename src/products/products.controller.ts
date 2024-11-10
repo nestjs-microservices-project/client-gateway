4ce0f5b38f269dd04d66a8fcbe5d23d2c758a9bc
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-11-10 20:46:06 +0200

    Call working for get products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,21 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PRODUCT_SERVICE } from 'src/config';
 
 @Controller('products')
 export class ProductsController {
-  constructor() {}
+  constructor(
+    @Inject(PRODUCT_SERVICE) private productsClient: ClientProxy 
+  ) {}
   
   @Post()
-  createProduct(@Body() createProductDto: CreateProductDto) {
+  createProduct() {
+
   }
 
   @Get()
-  findAllProducts(@Query() paginationDto: PaginationDto) {
- 
+  findAllProducts() {
+    return this.productsClient.send({ cmd: 'find_all' }, {});
   }
 
   @Get(':id')
@@ -23,8 +28,7 @@ export class ProductsController {
 
   @Patch(':id')
   patchProduct(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updateProductDto: UpdateProductDto,
+    @Param('id', ParseIntPipe) id: number
   ) {
     
   }
